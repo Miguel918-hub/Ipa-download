836352003d224be9a882f804f0bf762cd843a777
Miguel918-hub <migueldiscord970@gmail.com>
2025-09-18 16:51:39 -0300

    Update and rename connect.lua to config.lua

---
+++ config.lua
@@ -0,0 +1,8 @@
+-- Configurações globais do Nexus Hub
+local Config = {}
+
+Config.HubColor = Color3.fromRGB(128, 0, 255) -- roxo futurista
+Config.HubName = "☠️ Nexus Hub || 🔨 Admin"
+Config.RainbowSpeed = 0.5 -- velocidade do rainbow no nome
+
+return Config
